d59c2fc5e95debb5d932194643f21197c0097462
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-06 12:37:48 -0400

    Finished all static format methods, taking the parameters and successfully format them to the desired format

---
+++ src/main/java/com/plurasight/NameFormatter.java
@@ -0,0 +1,28 @@
+package com.plurasight;
+
+import java.util.ArrayList;
+
+public class NameFormatter {
+    private NameFormatter(){}
+
+    public static String format(String firstName, String lastName){
+        return String.format("%s %s",lastName,firstName);
+    }
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix){
+        return  String.format("%s, %s %s %s, %s", lastName,prefix,firstName,middleName,suffix);
+    }
+    public static String format(String fullName){
+        String[] suffixSplit = fullName.split(",");
+        String[] fullNameSplit = suffixSplit[0].split(" ");
+        String suffix = "";
+        if (suffixSplit.length > 1) {
+            suffix = ", " + suffixSplit[1].strip();
+        }
+        String lastName = fullNameSplit[fullNameSplit.length-1];
+        StringBuilder name = new StringBuilder();
+        for(int i = 0; i < fullNameSplit.length-1 ; i++){
+            name.append(fullNameSplit[i]).append(" ");
+        }
+        return String.format("%s %s%s", lastName,name,suffix);
+    }
+}
